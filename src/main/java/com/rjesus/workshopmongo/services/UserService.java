cf4e224e4635fdcc3989ea01e603c511bb4270d5
Rubens Jesus <jesus.rubens@gmail.com>
2018-10-16 16:16:33 -0300

    Obtendo Usuário por id

--- src/main/java/com/rjesus/workshopmongo/services/UserService.java
+++ src/main/java/com/rjesus/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rjesus.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rjesus.workshopmongo.domain.User;
 import com.rjesus.workshopmongo.repository.UserRepository;
+import com.rjesus.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 
 }
